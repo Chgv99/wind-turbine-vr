a4622faf847db1703a598730d1e4ef02122f4756
Christian García <christiangv99@gmail.com>
2022-04-25 15:01:05 +0100

    Fix #2

--- Assets/PlayerInputController.cs
+++ Assets/PlayerInputController.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class PlayerInputController : MonoBehaviour
 {
     public UnityEvent OpenPauseMenu;
     public UnityEvent ClosePauseMenu;
 
+    private XRIDefaultInputActions xri;
+    private InputAction pause;
+
     #region VAR
     [SerializeField] bool paused = false;
     #endregion
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        xri = new XRIDefaultInputActions();
     }
 
     // Update is called once per frame
@@ -24,7 +28,7 @@ public class PlayerInputController : MonoBehaviour
         
     }
 
-    public void Pause(){
+    public void Pause(InputAction.CallbackContext obj){
         if (paused) ClosePauseMenu?.Invoke();
         else OpenPauseMenu?.Invoke();
         paused = !paused;
@@ -33,4 +37,23 @@ public class PlayerInputController : MonoBehaviour
     /*public void Resume(){
         ClosePauseMenu?.Invoke();
     }*/
+
+    void OnEnable(){
+        //pause = xri.XRILeftHandInteraction.Pause;
+        //pause.Enable();
+        
+        xri.XRILeftHandInteraction.Pause.performed += Pause;
+        xri.XRILeftHandInteraction.Pause.Enable();
+
+        xri.XRIRightHandInteraction.Pause.performed += Pause;
+        xri.XRIRightHandInteraction.Pause.Enable();
+    }
+
+    void OnDisable(){
+        //pause = xri.XRILeftHandInteraction.Pause;
+        //pause.Enable();
+
+        xri.XRILeftHandInteraction.Pause.Disable();
+        xri.XRIRightHandInteraction.Pause.Disable();
+    }
 }
