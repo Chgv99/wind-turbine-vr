28f52d179c2f9f358e4d39e683aee35c6ff44998
Christian García <christiangv99@gmail.com>
2023-02-05 21:55:06 +0000

    Rope tests

---
+++ Assets/Scripts/Core/Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace WindTurbineVR.Core
+{
+    public static class Error
+    {
+        public static void LogException(string message)
+        {
+            Exception e = new Exception(message);
+            Debug.LogException(e);
+            Debug.Break();
+        }
+    }
+}
