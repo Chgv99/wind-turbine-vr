b362b5e96ac04d88053add88ac1e3a9c911cd419
Christian García <christiangv99@gmail.com>
2023-06-05 19:15:30 +0100

    Video test

---
+++ Assets/Scripts/Core/Video/VideoLoader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoLoader : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    [SerializeField] string url = "";
+
+    public string Url { get => url; set => url = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.url = url;
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.Prepare();
+    }
+}
